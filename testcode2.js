e332e5724cc21c12cfe520184af6b06c738dca2c
wen421 <113977048+wen421@users.noreply.github.com>
2022-11-09 01:27:22 +0800

    2

---
+++ testcode2.js
@@ -0,0 +1,12 @@
+
+console.log('typeof true', typeof true)
+console.log('typeof 30', typeof 30)
+console.log('typeof 0.5', typeof 0.5)
+console.log('typeof hello', typeof 'hello')
+console.log('typeof "30"', typeof "30")
+
+console.log('typeof [1]', typeof[1])
+console.log('type {a:1}', typeof {a:1})
+console.log('typeof null', typeof null)
+console.log('typeof undefined', typeof undefined)
+console.log('typeof function', typeof function(){})
